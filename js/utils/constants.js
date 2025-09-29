b39b503175fb60e838ce2187dde7d371d61e4a8f
JudgeTH <wunlapar@gmail.com>
2025-09-29 07:42:12 +0700

    Create constants.js

---
+++ js/utils/constants.js
@@ -0,0 +1,26 @@
+// js/utils/constants.js
+export const STORAGE_KEYS = {
+  DEVICE_ID: 'ecg_device_id',
+  CURRENT_UID: 'ecg_current_uid'
+};
+
+export const ECON = {
+  DAILY_STAR_CAP: 50,   // ตรงกับเอกสาร
+  STAR_PER_CORRECT: 1,
+  STAR_STREAK_BONUS: 1
+};
+
+export const ITEM_TYPES = {
+  HAT: 'hat',
+  OUTFIT: 'outfit',
+  PET: 'pet',
+  PET_FOOD: 'pet_food',
+  CONSUMABLE: 'consumable'
+};
+
+export const SLOTS = {
+  HAT: 'hat',
+  OUTFIT: 'outfit',
+  PET: 'pet',
+  CONSUMABLE: 'consumable'
+};
